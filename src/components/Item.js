ae34ecdbb07ad838195fc0d29c5d552525f724f6
Bridgett Wheeler <bridgettwheeler@gmail.com>
2021-09-14 12:00:31 -0400

    stopped for help

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,8 +1,18 @@
 import React from "react";
 
 function Item({ name, category }) {
+  function Button() {
+    const [isClicked, setIsOn] = useState(false);
+
+    function handleClick() {
+      setIsOn((isClicked) => !isClicked);
+    }
+    
+  }
+  const itemClass = isClicked ? "in-cart" : " "
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
       <button className="add">Add to Cart</button>
